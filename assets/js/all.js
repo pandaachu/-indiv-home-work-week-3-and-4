3d619d1854f52bb0d34c1e3049b7104aac904783
pandaachu <pandaachu@gmail.com>
2020-06-13 13:43:37 +0800

    Update 2020-06-13T05:43:28.018Z

---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var changeImg = document.querySelector('.changeImg');
+changeImg.style.transition = ' .5s ease-in-out';
+changeImg.addEventListener("mouseover", function () {
+  changeImg.src = '../assets/images/product-2-color.png';
+}, false);
+changeImg.addEventListener("mouseout", function () {
+  changeImg.src = '../assets/images/product-2.png';
+}, false);
+//# sourceMappingURL=all.js.map
