8206cc0fe4cbca62674ac9561487f904edfa522d
pandaachu <pandaachu@gmail.com>
2020-06-13 18:43:06 +0800

    improve img path

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,10 +3,10 @@ const changeImg = document.querySelector('.changeImg')
 changeImg.style.transition = ' .5s ease-in-out' 
 
 changeImg.addEventListener("mouseover", function () {
-  changeImg.src = '../assets/images/product-2-color.png'
+  changeImg.src = 'assets/images/product-2-color.png'
 }, false)
 
 changeImg.addEventListener("mouseout", function () {
-  changeImg.src = '../assets/images/product-2.png'
+  changeImg.src = 'assets/images/product-2.png'
 }, false)
 
